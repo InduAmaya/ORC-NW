1032bfb79f73ee9cdfa0761118aa99508e8fe005
InduAmaya <amayahewavithana@gmail.com>
2021-02-06 14:38:31 +0530

    home edited

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <div id="slide">
     <b-carousel
       id="carousel-fade"
@@ -13,14 +14,21 @@
     >
     <div class="carousel"> 
       <!-- Slides with image only -->
-      <b-carousel-slide  img-src="../assets/mount.jpg"></b-carousel-slide>
-      <b-carousel-slide  img-src="../assets/hiking.jpg"></b-carousel-slide>
-      <b-carousel-slide  img-src="../assets/rafting.jpg"></b-carousel-slide>
-      <b-carousel-slide  img-src="../assets/abseiling.jpg"></b-carousel-slide>
-      <b-carousel-slide  img-src="../assets/camp.jpg"></b-carousel-slide>
+      <b-carousel-slide  img-src="../assets/hiking.png"></b-carousel-slide>
+      <b-carousel-slide  img-src="../assets/rafting.png"></b-carousel-slide>
+      <b-carousel-slide  img-src="../assets/abseiling.png"></b-carousel-slide>
+      <b-carousel-slide  img-src="../assets/camping.png"></b-carousel-slide>
     </div>
     </b-carousel>
   </div>
+
+  <div id="text">
+    <b-card no-body class="text-left" border-variant="light">
+    <h2 class="h2">Welcome to ORC</h2>
+    <p class="para">Explore amazing adventurous activites and plan your adventure.</p>
+  </b-card>
+  </div>
+</div>
 </template>
 
 <script>
@@ -56,5 +64,19 @@ export default {
   width: 100%;
   padding-top: 0%;
   margin-top: 0%;
+}
+
+#text{
+  max-width: 50%;
+  margin-top: 30px;
+  padding-left: 30px;
+}
+
+.h2{
+  font-size: 48pt;
+}
+
+.para{
+  font-size: 24pt;
 }
 </style>
